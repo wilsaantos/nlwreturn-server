1cf03ffae66fe01b0ebb9d835efe871bf24e9bf8
Wilson Santos <wilson.saantos@hotmail.com>
2022-05-06 18:51:29 -0300

    Stage 5 - Setup for Deploy

--- src/services/feedback-service.ts
+++ src/services/feedback-service.ts
@@ -40,6 +40,7 @@ export class FeedbackService {
                 `<div style="font-family: sans serif; font-size: 16px; color: #111;">`,
                 `<p>Tipo do feedback: ${type}<p>`,
                 `<p>Comentário: ${comment}<p>`,
+                screenshot ? `<img src="${screenshot}" />` : '',
                 `</div>`
             ].join('\n')
         })
